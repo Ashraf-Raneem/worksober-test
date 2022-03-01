393b0a96604915a2aa5cad2033b0638cbe104e7b
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-03-01 18:08:30 +0600

    user actions added to react app

---
+++ worksober-frontend/src/redux/user/UserActions.js
@@ -0,0 +1,59 @@
+import axios from "axios";
+import { CREATE_USER, DELETE_USER, GET_USERS, LOADING, UPDATE_USER } from "./UserActionTypes";
+import { url } from "../../Utils";
+
+export const getUsers = () => (dispatch) => {
+  dispatch(loading());
+  axios
+    .get(url + `/users`)
+    .then((res) => {
+      dispatch({
+        type: GET_USERS,
+        payload: res.data.data,
+      });
+    })
+    .catch((err) => {});
+};
+
+export const postUser = (formData) => (dispatch) => {
+  dispatch(loading());
+  axios
+    .post(url + "/users", formData)
+    .then((res) => {
+      dispatch({
+        type: CREATE_USER,
+      });
+    })
+    .catch((err) => {});
+};
+
+export const updateUser = (formData, id) => (dispatch) => {
+  dispatch(loading);
+  axios
+    .put(url + `/users/${id}`, formData)
+    .then((res) => {
+      dispatch({
+        type: UPDATE_USER,
+      });
+      console.log(res.data);
+    })
+    .catch((err) => {});
+};
+
+export const deleteUser = (id) => (dispatch) => {
+  dispatch(loading);
+  axios
+    .delete(url + `/users/${id}`)
+    .then((res) => {
+      dispatch({
+        type: DELETE_USER,
+      });
+    })
+    .catch((err) => {});
+};
+
+const loading = () => {
+  return {
+    type: LOADING,
+  };
+};
---
+++ worksober-frontend/src/redux/user/UserReducer.js
@@ -0,0 +1,39 @@
+import { CREATE_USER, DELETE_USER, GET_USERS, LOADING, UPDATE_USER } from "./UserActionTypes";
+
+const INITIAL_STATE = {
+  users: null,
+  loading: false,
+};
+
+export default function userReducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+    case GET_USERS:
+      return {
+        ...state,
+        users: action.payload,
+        loading: false,
+      };
+    case CREATE_USER:
+      return {
+        ...state,
+        loading: false,
+      };
+    case UPDATE_USER:
+      return {
+        ...state,
+        loading: false,
+      };
+    case DELETE_USER:
+      return {
+        ...state,
+        loading: false,
+      };
+    case LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    default:
+      return state;
+  }
+}
