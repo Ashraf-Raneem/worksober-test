28d3285ad4fc011793cff9ceef78191568f29cc2
Ashraf-Raneem <ashrafraneem@gmail.com>
2022-03-07 01:47:02 +0600

    user details added

--- worksober-frontend/src/components/UserCard.js
+++ worksober-frontend/src/components/UserCard.js
@@ -1,13 +1,16 @@
 import React, { useState } from "react";
 import Modal from "react-modal";
 import { MdUpdate, MdOutlineCancel } from "react-icons/md";
+import { AiFillEye } from "react-icons/ai";
 import { BiTrash } from "react-icons/bi";
 import UpdateUserForm from "./modals/UpdateUserForm";
 import { deleteUser } from "../redux/user/UserActions";
 import { connect } from "react-redux";
+import UserDetails from "./modals/UserDetails";
 
 const UserCard = ({ user, deleteUser }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [detailOpen, setDetailOpen] = useState(false);
 
   const customStyles = {
     content: {
@@ -22,12 +25,28 @@ const UserCard = ({ user, deleteUser }) => {
       transform: "translate(-50%, -50%)",
     },
   };
+  const customStylessm = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      padding: 0,
+      minHeight: "250px",
+      minWidth: "30%",
+      borderRadius: "25px",
+      transform: "translate(-50%, -50%)",
+    },
+  };
 
   const toggle = () => {
     setIsOpen(!isOpen);
   };
 
-  console.log(user);
+  const toggleDetail = () => {
+    setDetailOpen(!detailOpen);
+  };
 
   return (
     <React.Fragment>
@@ -56,10 +75,14 @@ const UserCard = ({ user, deleteUser }) => {
             Email : <strong>{user.email}</strong>
           </p>
         </div>
-        <div>
+        <div className="btn-group">
           <button className="btn primary-btn" onClick={toggle}>
             <MdUpdate size={16} style={{ marginRight: "5px" }} />
             <span>Update</span>
+          </button>
+          <button className="btn secondary-btn" onClick={toggleDetail} style={{ marginLeft: "10px" }}>
+            <AiFillEye size={16} style={{ marginRight: "5px" }} />
+            <span>Details</span>
           </button>
         </div>
       </div>
@@ -71,6 +94,37 @@ const UserCard = ({ user, deleteUser }) => {
           </div>
           <div className="modal-body">
             <UpdateUserForm user={user} toggle={toggle} />
+          </div>
+        </div>
+      </Modal>
+      <Modal isOpen={detailOpen} style={customStylessm} ariaHideApp={false}>
+        <div>
+          <div className="modal-header-alt">
+            <div className="user-details-header">
+              <img src={user.picture} alt="user" />
+              <div className="user-details-header-sub">
+                <h3>{user.name}</h3>
+                <span style={{ marginLeft: "5px" }}>Club member</span>
+              </div>
+            </div>
+            <MdOutlineCancel size={20} style={{ cursor: "pointer" }} onClick={toggleDetail} />
+          </div>
+          <UserDetails user={user} toggle={toggleDetail} />
+          <div className="user-details-footer">
+            <div className="btn-group">
+              <button
+                className="btn primary-btn"
+                onClick={() => {
+                  toggleDetail();
+                  toggle();
+                }}
+              >
+                <span>Update User</span>
+              </button>
+              <button className="btn secondary-btn" onClick={toggleDetail}>
+                <span>Cancel</span>
+              </button>
+            </div>
           </div>
         </div>
       </Modal>
